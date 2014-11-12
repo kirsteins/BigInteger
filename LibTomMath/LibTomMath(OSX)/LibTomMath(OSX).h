949175f81706e7219f531358817af913af3df737
Jānis Kiršteins <janis@tawk.to>
2014-11-12 13:21:08 +0200

    add OSX targets

---
+++ LibTomMath/LibTomMath(OSX)/LibTomMath(OSX).h
@@ -0,0 +1,19 @@
+//
+//  LibTomMath(OSX).h
+//  LibTomMath(OSX)
+//
+//  Created by Jānis Kiršteins on 12/11/14.
+//  Copyright (c) 2014 Jānis Kiršteins. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LibTomMath(OSX).
+FOUNDATION_EXPORT double LibTomMath_OSX_VersionNumber;
+
+//! Project version string for LibTomMath(OSX).
+FOUNDATION_EXPORT const unsigned char LibTomMath_OSX_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibTomMath_OSX_/PublicHeader.h>
+
+
--- Tests/BigIntegerTests.swift
+++ Tests/BigIntegerTests.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Jānis Kiršteins. All rights reserved.
 //
 
-import UIKit
+//import Foundaution
 import XCTest
 import BigInteger
 
