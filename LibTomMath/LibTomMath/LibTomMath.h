f985e44499320e004204b3ed1803f2fb94317d79
Jānis Kiršteins <janis@tawk.to>
2014-11-12 12:22:33 +0200

    add LibTomMath project

---
+++ LibTomMath/LibTomMath/LibTomMath.h
@@ -0,0 +1,19 @@
+//
+//  LibTomMath.h
+//  LibTomMath
+//
+//  Created by Jānis Kiršteins on 12/11/14.
+//  Copyright (c) 2014 Jānis Kiršteins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibTomMath.
+FOUNDATION_EXPORT double LibTomMathVersionNumber;
+
+//! Project version string for LibTomMath.
+FOUNDATION_EXPORT const unsigned char LibTomMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibTomMath/PublicHeader.h>
+
+
